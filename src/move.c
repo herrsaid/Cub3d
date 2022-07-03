8d058778051798d147690bf8967cf25e452b816b
Salah eddine Elhandaoui <selhanda@e1r4p3.1337.ma>
2022-07-03 12:00:58 +0100

    draw rays

--- src/move.c
+++ src/move.c
@@ -14,42 +14,28 @@
 
 int	move_f(int keycode, t_data *data)
 {
+    int i = 0;
+    int x = 0;
+    int y = 0;
 	if (keycode == 0x35)
 		close_win(data);
 	if (keycode == 0x7C || keycode == 0x02)
-	{
-		if (!move_right(data))
-		{
-            ft_display(data->player->player_x, data->player->player_y, data, 16711680, 12);
-            draw_line(data, data->player->player_x, data->player->player_y, 70, 70);
-		}
-			
-	}
+	    move_right(data);
 	else if (keycode == 0x7B || keycode == 0x00)
-	{
-		if (!move_left(data))
-		{
-            ft_display(data->player->player_x, data->player->player_y, data, 16711680, 12);
-            draw_line(data, data->player->player_x, data->player->player_y, 70, 70);
-		}
-	}
+	    move_left(data);
 	else if (keycode == 0x7D || keycode == 0x01)
-	{
-		if (!move_up(data))
-		{
-            ft_display(data->player->player_x, data->player->player_y, data, 16711680, 12);
-            draw_line(data, data->player->player_x, data->player->player_y, 70, 70);
-		}
-	}
+	    move_up(data);
 	else if (keycode == 0x7E || keycode == 0x0D)
-	{
-		if (!move_down(data))
-		{
-            ft_display(data->player->player_x, data->player->player_y, data, 16711680, 12);
-            draw_line(data, data->player->player_x, data->player->player_y, 70, 70);
-		}
-	}
+	    move_down(data);
     ft_drwa2dmap(data);
+    while (i < 33)
+    {
+        draw_line(data, data->player->player_x , data->player->player_y + 6, 0 , data->player->player_y - y);
+        i++;
+        x += 1;
+        y += 10;
+    }
+    ft_display(data->player->player_x, data->player->player_y, data, 16711680, 12);
     return (0);
 }
 
