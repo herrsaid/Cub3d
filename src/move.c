0bbfd4f7aababf427437140003754ddcc25dc2b6
Said Lamane <salamane@e1r5p9.1337.ma>
2022-08-02 10:57:02 +0100

    scale down move

--- src/move.c
+++ src/move.c
@@ -34,10 +34,10 @@ int	r_right(t_data *data)
     // data->player->pa += 0.1;
 //     if (data->player->pa > 2 * PI)
 //         data->player->pa -= 2 * PI;
-    data->player->pdx = cos(data->player->pa) * 12;
-    data->player->pdy = sin(data->player->pa) * 12;
+    data->player->pdx = cos(data->player->pa) * 6;
+    data->player->pdy = sin(data->player->pa) * 6;
     // return (0);
-    data->player->pa += 1 * degtorad(8);
+    data->player->pa += 1 * degtorad(5);
     return (0);
 }
 
@@ -46,16 +46,16 @@ int	r_left(t_data *data)
     // data->player->pa -= 0.1;
 //     if (data->player->pa < 0)
 //         data->player->pa += 2 * PI;
-     data->player->pdx = cos(data->player->pa) * 12;
-     data->player->pdy = sin(data->player->pa) * 12;
-    data->player->pa += -1 * degtorad(8);
+     data->player->pdx = cos(data->player->pa) * 6;
+     data->player->pdy = sin(data->player->pa) * 6;
+    data->player->pa += -1 * degtorad(5);
 	return (0);
 }
 
 int	move_down(t_data *data)
 {
-    float newx = data->player->player_x + cos(data->player->pa) * 12;
-    float newy = data->player->player_y + sin(data->player->pa) * 12;
+    float newx = data->player->player_x + cos(data->player->pa) * 6;
+    float newy = data->player->player_y + sin(data->player->pa) * 6;
     if (data->map[(int)newy / 32][(int)newx / 32] != '1')
     {
         data->player->player_x = newx;
@@ -66,8 +66,8 @@ int	move_down(t_data *data)
 
 int	move_up(t_data *data)
 {
-    float newx = data->player->player_x - cos(data->player->pa) * 12;
-    float newy = data->player->player_y - sin(data->player->pa) * 12;
+    float newx = data->player->player_x - cos(data->player->pa) * 6;
+    float newy = data->player->player_y - sin(data->player->pa) * 6;
     if (data->map[(int)newy / 32][(int)newx / 32] != '1')
     {
         data->player->player_x = newx;
